c4dd968f6037127505387438f7ad21f9b9675e2a
Ryan Kavanagh <ryanakca@kubuntu.org>
2010-07-19 15:05:23 -0400

    Time picker works

--- header.php
+++ header.php
@@ -1,8 +1,8 @@
 <?php
 echo '<link type="text/css" rel="stylesheet" href="css/jquery-ui-1.8.2.custom.css" />
 <link type="text/css" rel="stylesheet" href="css/et.css" />
-<script type="text/JavaScript" src="js/jquery-1.4.2.min.js"></script> 
-<script type="text/JavaScript" src="js/jquery-ui-1.8.2.custom.min.js"></script>
-<script type="text/JavaScript" src="js/jquery-ui-timepicker-addon-0.4.js"></script>
+<script type="text/javascript" src="http://code.jquery.com/jquery-latest.min.js"></script>
+<script type="text/javascript" src="js/jquery-ui-1.8.2.custom.min.js"></script>
+<script type="text/javascript" src="js/jquery-ui-timepicker-addon-0.5.js"></script>
 ';
 ?>
