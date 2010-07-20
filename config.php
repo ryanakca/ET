2781cd95193e9e5cf0d81300851a2af8b93bcfc2
Ryan Kavanagh <ryanakca@kubuntu.org>
2010-07-20 10:04:45 -0400

    Adding time for repetition works

--- config.php
+++ config.php
@@ -3,4 +3,6 @@
 $db_filename = 'et_db.txt';
 // Seperator to use in the database
 $sep = '';
+// DateTime format
+$dt_fmt = 'o-m-d\TH:i';
 ?>
--- et-so.php
+++ et-so.php
@@ -9,9 +9,9 @@ $i_name   = $_POST['item_name'];
 $i_desc   = $_POST['item_description'];
 $b_name   = $_POST['borrower_name'];
 $b_netid  = $_POST['borrower_netid'];
-$b_time   = date('o-m-d\TH:i', strtotime($_POST['pickup_datetime']));
+$b_time   = new DateTime('@'.strtotime($_POST['pickup_datetime']));
 // See below for setting repetition
-$d_time   = date('o-m-d\TH:i', strtotime($_POST['due_datetime']));
+$d_time   = new DateTime('@'.strtotime($_POST['due_datetime']));
 
 $empty_check = implode('', $_POST);
 if (empty($empty_check)) {
@@ -20,9 +20,13 @@ if (empty($empty_check)) {
     // Set $b_rep to 1 if not null;
     $b_rep = (isset($_POST['repetition']) && !empty($_POST['repetition'])) ? $_POST['repetition'] : 1;
     for ($rep = 0; $rep < $b_rep; $rep++) {
-        $entry = $i_name . $sep . $i_desc . $sep . $b_name . $sep . $b_netid . $sep . $b_time . $sep . $d_time . "\n";
+        $entry = $i_name . $sep . $i_desc . $sep . $b_name . $sep . $b_netid . $sep . $b_time->format($dt_fmt) . $sep . $d_time->format($dt_fmt) . "\n";
         $escaped_entry = htmlspecialchars($entry);
         fwrite($fdb, $escaped_entry);
+        // Increment time by one week for next pass/repetition. P1W -> Period of
+        // 1 Week. See ISO8601
+        $b_time->add(new DateInterval('P1W'));
+        $d_time->add(new DateInterval('P1W'));
     }
     fclose($fdb);
     echo header('Location: et-lo.php');
