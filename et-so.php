ca835a9c3bb93e78db4891a94f495f6341417ba2
Ryan Kavanagh <ryanakca@kubuntu.org>
2010-07-20 08:29:02 -0400

    Added pickup date/time and moved due date/time to item fieldset

--- et-so.php
+++ et-so.php
@@ -9,15 +9,14 @@ $i_name   = $_POST['item_name'];
 $i_desc   = $_POST['item_description'];
 $b_name   = $_POST['borrower_name'];
 $b_netid  = $_POST['borrower_netid'];
-$b_time   = $_POST['pickup_time'];
-$d_date   = $_POST['due_date'];
-$d_time   = $_POST['due_time'];
+$b_time   = $_POST['pickup_datetime'];
+$d_time   = $_POST['due_datetime'];
 
 $empty_check = implode('', $_POST);
 if (empty($empty_check)) {
     die('ERROR: Submitted an empty form!');
 } else {
-    $entry = $i_name . $sep . $i_desc . $sep . $b_name . $sep . $b_netid . $sep . $b_time . $sep . $d_date . $sep . $d_time . "\n";
+    $entry = $i_name . $sep . $i_desc . $sep . $b_name . $sep . $b_netid . $sep . $b_time . $sep . $d_time . "\n";
     $escaped_entry = htmlspecialchars($entry);
     fwrite($fdb, $escaped_entry);
     fclose($fdb);
