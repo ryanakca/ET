0c08b51af997fe468d9115bda02792a22e5df197
Ryan Kavanagh <ryanakca@kubuntu.org>
2010-07-21 14:29:18 -0400

    Updated config file with table names

--- config.php
+++ config.php
@@ -1,8 +1,13 @@
 <?php
 // Database filename
 $db_filename = 'et_db.txt';
-// Seperator to use in the database
-$sep = '';
+// Seperators to use in the database
+$table_sep = '=.=.=';
+$cell_sep = '';
 // DateTime format
 $dt_fmt = 'o-m-d H:i';
+
+// Table names
+$loans_table = 'Loans';
+$objects_table = 'Objects';
 ?>
