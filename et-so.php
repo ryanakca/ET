e3fa5fc1c86b0a263383c1794473cdbafcbf0eb7
Ryan Kavanagh <ryanakca@kubuntu.org>
2010-07-21 14:35:23 -0400

    Updated et-so.php to work with the new FlatFileDB

--- et-so.php
+++ et-so.php
@@ -2,8 +2,9 @@
 // Open the database
 
 include 'config.php';
+include 'db.php';
 
-$fdb = fopen($db_filename, 'a') or die('ERROR: Unable to open database for appending!');
+$fdb = new FlatFileDB($db_filename, $table_sep, $cell_sep);
 
 $i_name   = $_POST['item_name'];
 $i_desc   = $_POST['item_description'];
@@ -20,15 +21,14 @@ if (empty($empty_check)) {
     // Set $b_rep to 1 if not null;
     $b_rep = (isset($_POST['repetition']) && !empty($_POST['repetition'])) ? $_POST['repetition'] : 1;
     for ($rep = 0; $rep < $b_rep; $rep++) {
-        $entry = $i_name . $sep . $i_desc . $sep . $b_name . $sep . $b_netid . $sep . $b_time->format($dt_fmt) . $sep . $d_time->format($dt_fmt) . "\n";
+        $entry = $i_name . $cell_sep . $i_desc . $cell_sep . $b_name . $cell_sep . $b_netid . $cell_sep . $b_time->format($dt_fmt) . $cell_sep . $d_time->format($dt_fmt) . "\n";
         $escaped_entry = htmlspecialchars($entry);
-        fwrite($fdb, $escaped_entry);
+        $fdb->newRow($loans_table, $entry);
         // Increment time by one week for next pass/repetition. P1W -> Period of
         // 1 Week. See ISO8601
         $b_time->add(new DateInterval('P1W'));
         $d_time->add(new DateInterval('P1W'));
     }
-    fclose($fdb);
     echo header('Location: et-lo.php');
 }
 
