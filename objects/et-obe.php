0105bb3cdb7d228f6b7888f65bf1f92c85e1bd41
Ryan Kavanagh <ryanakca@kubuntu.org>
2010-07-22 14:39:41 -0400

    Edit objects

---
+++ objects/et-obe.php
@@ -0,0 +1,21 @@
+<?php
+// Open the database
+
+include '../config.php';
+include '../db.php';
+
+
+$i_name   = $_POST['item_name'];
+
+$empty_check = implode('', $_POST);
+if (empty($empty_check)) {
+    die('ERROR: Submitted an empty form!');
+} else {
+    $fdb = new FlatFileDB($db_filename, $table_sep, $cell_sep);
+    $entry = array(htmlspecialchars($i_name));
+    $fdb->editRow($objects_table, $erow, $entry);
+    echo header('Location: view.html');
+}
+
+?>
+
