467850ddd2a8de87604becbe13273e5ffbdb94d4
Ryan Kavanagh <ryanakca@kubuntu.org>
2010-07-22 14:36:40 -0400

    Fix typo in add.php for objects

--- objects/add.php
+++ objects/add.php
@@ -11,9 +11,9 @@ if (empty($empty_check)) {
     die('ERROR: Submitted an empty form!');
 } else {
     $i_name = $_POST['item_name'];
-    $entry = array(htmltospecialchars($i_name));
+    $entry = array(htmlspecialchars($i_name));
     $fdb->newRow($objects_table, $entry);
-    echo header('Location: view.php');
+    echo header('Location: view.html');
 }
 
 ?>
