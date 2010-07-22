a6874f70379ed5ec6c63d3e449b635dfae0cd7e7
Ryan Kavanagh <ryanakca@kubuntu.org>
2010-07-22 10:23:06 -0400

    Delete row with new DB format works

--- et-de.php
+++ et-de.php
@@ -1,18 +1,19 @@
 <?php
 
+include 'config.php';
+include 'db.php';
+
 $empty_check = implode('', $_POST);
 if (empty($empty_check)) {
     echo header('Location: et-lo.php');
 } else {
     // $_POST['row'] is in the format: delrowX
     // where X is the row we want to delete.
-    $drow = (int)substr($_POST['row'], 6);
+    $row = (int)substr($_POST['row'], 6);
 
-    $db_lines = file('et_db.txt');
+    $fdb = new FlatFileDB($db_filename, $table_sep, $cell_sep);
 
-    unset($db_lines[$drow]);
-    $fdb = fopen('et_db.txt', 'w'); 
-    fwrite($fdb, join('',$db_lines));
+    $fdb->deleteRow($loans_table, $row);
 }
 
 ?>
