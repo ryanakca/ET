891d56a2898b3096c208e40bfe2b75125263fe62
Ryan Kavanagh <ryanakca@kubuntu.org>
2010-07-23 14:29:33 -0400

    Change jquery ui theme

--- header.php
+++ header.php
@@ -1,6 +1,6 @@
 <?php
 include 'config.php';
-echo '<link type="text/css" rel="stylesheet" href="' .  $base_path . 'css/jquery-ui-1.8.2.custom.css" />
+echo '<link type="text/css" rel="stylesheet" href="' .  $base_path . 'css/jquery-ui-1.8.custom.css" />
 <link type="text/css" rel="stylesheet" href="' .  $base_path . 'css/et.css" />
 <script type="text/javascript" src="http://code.jquery.com/jquery-latest.min.js"></script>
 <script type="text/javascript" src="' .  $base_path . 'js/jquery-ui-1.8.2.custom.min.js"></script>
