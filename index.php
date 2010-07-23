cb337f1b73981a8b44a915965b3263163a967505
Ryan Kavanagh <ryanakca@kubuntu.org>
2010-07-23 15:14:02 -0400

    Added an index page

---
+++ index.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN"
+"http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+<title>Equipment tracker</title>
+<meta http-equiv="Content-Type" content="text/html;charset=utf-8" />
+<style type="text/css">
+    #ui-timepicker-div dl{ text-align: left; }
+    #ui-timepicker-div dl dt{ height: 25px; }
+    #ui-timepicker-div dl dd{ margin: -25px 0 10px 65px; }
+</style>
+<?php include '../header.php'; ?>
+</head>
+<body>
+<h1>Equipment Tracker</h1>
+<h2>Public interface</h2>
+<a href="pub/pub.php">View signed out and available items</a>
+<h2>Admin interface</h2>
+<?php include 'footer.php' ?>
+</body>
+</html>
