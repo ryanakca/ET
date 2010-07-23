30ce9f1057f289b9480bc8b88d6b0e71391629bc
Ryan Kavanagh <ryanakca@kubuntu.org>
2010-07-23 10:18:35 -0400

    Fix object editing

--- objects/et-obe.php
+++ objects/et-obe.php
@@ -13,6 +13,7 @@ if (empty($empty_check)) {
 } else {
     $fdb = new FlatFileDB($db_filename, $table_sep, $cell_sep);
     $entry = array(htmlspecialchars($i_name));
+    $erow = substr($_POST['editrow'], 7);
     $fdb->editRow($objects_table, $erow, $entry);
     echo header('Location: view.html');
 }
