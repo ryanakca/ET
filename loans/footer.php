274ad849c8b522b2d51e5cf7b551cfe2043a3c95
Ryan Kavanagh <ryanakca@kubuntu.org>
2010-07-23 15:11:06 -0400

    Moved footer to central location

--- loans/footer.php
+++
@@ -1,6 +0,0 @@
-<?php
-echo '<p><a href="sign-out.html">Sign out items</a></p>
-<p><a href="view-out.html">View signed out items</a></p>
-<p><a href="../objects/add.html">Add items to DB</a></p>
-<p><a href="../objects/view.html">View items in DB</a></p>';
-?>
